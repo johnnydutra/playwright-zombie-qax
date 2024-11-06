6ce6286787adc16830fc2da6438b08e8ba5751a9
Johnny Dutra <johnny.dutra@gmail.com>
2024-11-06 14:33:49 -0300

    through lecture 46

--- pages/MoviesPage.js
+++ pages/MoviesPage.js
@@ -12,15 +12,27 @@ export class MoviesPage {
     await expect(logoutLink).toBeVisible();
   }
 
-  async add(title, overview, company, release_year) {
+  async accessForm() {
     await this.page.locator('a[href$="/register"]').click();
+  }
+
+  async submitForm() {
+    await this.page.getByRole('button', { name: 'Cadastrar' }).click();
+  }
+
+  async add(title, overview, company, release_year) {
+    await this.accessForm();
     await this.page.getByLabel('Titulo do filme').fill(title);
     await this.page.getByLabel('Sinopse').fill(overview);
     await this.page.locator('#select_company_id .react-select__indicator').click();
     await this.page.locator('.react-select__option').filter({ hasText: company }).click();
     await this.page.locator('#select_year .react-select__indicator').click();
     await this.page.locator('.react-select__option').filter({ hasText: release_year }).click();
-    await this.page.getByRole('button', { name: 'Cadastrar' }).click();
+    await this.submitForm();
   }
 
+  async alertHasText(text) {
+    const alert = this.page.locator('span[class$="alert"]');
+    await expect(alert).toHaveText(text);
+  }
 }
--- tests/e2e/movies.spec.js
+++ tests/e2e/movies.spec.js
@@ -1,5 +1,5 @@
 const { test } = require('../../support');
-import { executeSQL } from '../../support/database';
+import { executeSQL } from '../../support/helpers/database';
 const data = require('../../support/fixtures/movies.json');
 
 test('should add a new movie', async ({ page }) => {
@@ -12,4 +12,19 @@ test('should add a new movie', async ({ page }) => {
 
   await page.movies.add(movie.title, movie.overview, movie.company, movie.release_year);
   await page.toast.containsText('Cadastro realizado com sucesso!');
+});
+
+test('should not add new movie with unfilled mandatory fields', async ({ page }) => {
+  await page.login.visit();
+  await page.login.submitForm('admin@zombieplus.com', 'pwd123');
+  await page.movies.isLoggedIn();
+
+  await page.movies.accessForm();
+  await page.movies.submitForm();
+  await page.movies.alertHasText([
+    'Por favor, informe o título.',
+    'Por favor, informe a sinopse.',
+    'Por favor, informe a empresa distribuidora.',
+    'Por favor, informe o ano de lançamento.'
+  ]);
 });
